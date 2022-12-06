cece0f1fbecd8baf364448c42978084d5a78332d
sauravgupta2800 <sauravgupta2800@gmail.com>
2022-12-05 22:59:27 -0800

    changd the text

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>Coding Patterns</p>
+        <p>Coding Patterns coming soon</p>
       </header>
     </div>
   );
